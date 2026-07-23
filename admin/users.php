eb285dfb2c924a15dc47648b615d686561593ed5
AishwaryaPrashant <aishwaryaprashant04@gmail.com>
2026-07-23 20:21:43 +0545

    asd

--- admin/users.php
+++ admin/users.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 require_once __DIR__ . '/../config/db.php';
-require_once __DIR__ . '/../includes/functions.php';
+
 require_once __DIR__ . '/../includes/header.php';
 
 if(isset($_POST['recharge_balance'])){
